46382c527a623b76f4e36cf602e962a0900895ce
Mao <maurycy.niewczas@gmail.com>
2022-10-13 21:22:53 +0200

    raise exception feature

--- game/board.py
+++ game/board.py
@@ -1,3 +1,4 @@
+from ast import While
 import numpy as np
 from my_enums import PionColor
 from pion import Pion
@@ -22,12 +23,33 @@ class Board:
             print()
 
     def set_field(self, x_coord: int, y_coord: int, color: PionColor):
+        if x_coord < 0 > y_coord or x_coord >= self.__size <= y_coord:
+            raise IndexError(
+                "Bad coordinates, coordinates must be beetwen <0,8>")
+        elif self.__coordinates[y_coord][x_coord].pion_color != PionColor.EMPTY:
+            raise ValueError("Bad coordinates, can't overwrite this field")
         self.__coordinates[y_coord][x_coord].pion_color = color
 
 
 if __name__ == "__main__":
     board = Board(9)
-    board.set_field(0, 2, PionColor.BLACK)
-    board.display_board()
-    board.set_field(1, 2, PionColor.WHITE)
-    board.display_board()
+    turn = 0
+
+    while True:
+        print("czarny") if turn % 2 == 0 else print("bialy")
+        x = int(input("podaj x: "))
+        if x == 420:
+            break
+        y = int(input("podaj y: "))
+        try:
+            if turn % 2 == 0:
+                board.set_field(x, y, PionColor.BLACK)
+                board.display_board()
+            else:
+                board.set_field(x, y, PionColor.WHITE)
+                board.display_board()
+            turn += 1
+        except IndexError as e:
+            print(e)
+        except ValueError as e:
+            print(e)
