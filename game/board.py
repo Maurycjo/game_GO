70b74c8daffb6262a7d4a70d7c5a284cbb7bd076
Mao <maurycy.niewczas@gmail.com>
2022-10-13 20:04:26 +0200

    change from numpy to list

--- game/board.py
+++ game/board.py
@@ -1,4 +1,5 @@
 import numpy as np
+from my_enums import PionColor
 from pion import Pion
 
 
@@ -6,7 +7,8 @@ class Board:
 
     def __init__(self, size):
         self.__size = size
-        self.__coordinates = np.full((self.__size, self.__size,), Pion())
+        self.__coordinates = [[Pion() for i in range(self.__size)]
+                              for j in range(self.__size)]
 
     def display_board(self):
         print(" ", end="")
@@ -19,7 +21,13 @@ class Board:
                 print(" " + pion.get_pion_symbol(), end="")
             print()
 
+    def set_field(self, x_coord: int, y_coord: int, color: PionColor):
+        self.__coordinates[y_coord][x_coord].pion_color = color
+
 
 if __name__ == "__main__":
     board = Board(9)
+    board.set_field(0, 2, PionColor.BLACK)
+    board.display_board()
+    board.set_field(1, 2, PionColor.WHITE)
     board.display_board()
